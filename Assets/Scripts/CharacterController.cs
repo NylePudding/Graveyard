80436f42ce19e2d36219c05df6c447c71dd52757
NylePudding <eeu41f@bangor.ac.uk>
2016-08-12 17:52:42 +0100

    Setting up rep

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+
+        print("trans : " + translation + " straffe : " + straffe);
+
+
+        if (translation > 0 && straffe > 0 ) {
+            translation += Time.deltaTime;
+            straffe += Time.deltaTime;
+        }
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape")) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+}
---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+	public GameObject graveStone0;
+	public GameObject graveStone1;
+	public GameObject graveStone2;
+	public GameObject graveStone3;
+	private GameObject [,] graveyard;
+
+	// Use this for initialization
+	void Start () {
+	
+		generateGraveyard();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void generateGraveyard(){
+
+		graveyard = new GameObject[6,6];
+
+		for(int i = 0; i < 6; i ++){
+			for (int j = 0; j < 6; j ++){
+
+				GameObject gs = getRandomGravestone();
+				Vector3 pos = new Vector3(i*8f,0.2f,j*6f);
+				Quaternion rot = Quaternion.AngleAxis(-10f,Vector3.up);
+
+				graveyard[i,j] = gs;
+
+				Instantiate(gs,pos,Quaternion.identity);
+			}
+		}
+	}
+
+
+	GameObject getRandomGravestone(){
+
+		GameObject gs = graveStone0;
+
+		int graveno = Random.Range(0,4);
+
+		switch(graveno){
+		case 0:
+			gs = graveStone0;
+			break;
+		case 1:
+			gs = graveStone1;
+			break;
+		case 2:
+			gs = graveStone2;
+			break;
+		case 3:
+			gs = graveStone3;
+			break;
+		}
+
+		return gs;
+		
+	}
+
+}
